095fd06f1c764c554a44fce64ca9d6a0f30d6125
its-me-bauddha <harshbauddha563@gmail.com>
2023-11-03 14:38:24 +0530

    SignIn SignUp Validation done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,33 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { checkValidData, checkValidateName } from "../utils/validate";
 
 const Form = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const firstName = useRef(null);
+  const lastName = useRef(null);
+  const handleButtonClick = () => {
+    // Validate the form data
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    if (!isSignInForm) {
+      const message = checkValidateName(
+        firstName.current.value,
+        lastName.current.value
+      );
+      setErrorMessage(message);
+    }
+  };
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
   };
   return (
     <div>
       <form
-        className="absolute flex flex-col right-0 left-[400px] mt-[200px] w-80
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute flex flex-col right-0 left-[400px] mt-[150px] w-96
         m-40 p-5 text-white bg-black rounded-lg bg-opacity-80"
       >
         <h1 className="text-3xl p-2 text-white">
@@ -17,11 +36,13 @@ const Form = () => {
         {!isSignInForm && (
           <div className=" flex ">
             <input
+              ref={firstName}
               className="p-3 m-2 rounded-lg bg-richblack-800 w-[95%] "
               type="text"
               placeholder="First Name"
             />
             <input
+              ref={lastName}
               className="p-3 m-2 rounded-lg bg-richblack-800 w-[95%] "
               type="text"
               placeholder="Last Name"
@@ -29,17 +50,23 @@ const Form = () => {
           </div>
         )}
         <input
+          ref={email}
           className="p-3 m-2 rounded-lg bg-richblack-800 w-[95%] "
           type="text"
           placeholder="Email Address"
         />
 
         <input
+          ref={password}
           className="p-3 m-2 rounded-lg bg-richblack-800 w-[95%] "
           type="password"
           placeholder="Password"
         />
-        <button className="m-3 p-3 bg-red-5 w-[95%] rounded-md">
+        <p className="text-red-5 m-3 font-semibold text-lg">{errorMessage}</p>
+        <button
+          className="m-3 p-3 bg-red-5 w-[95%] rounded-md"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="m-3  text-pure-greys-100">
